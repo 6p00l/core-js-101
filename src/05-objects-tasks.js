e0224db8f8f4d412881005c12eabce11901a715f
6p00l <nolf91@gmail.com>
2022-07-23 04:00:47 +0300

    Add files via upload

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -57,8 +57,8 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.setPrototypeOf(JSON.parse(json), proto);
 }
 
 
@@ -116,37 +116,127 @@ function fromJSON(/* proto, json */) {
  *  For more examples see unit tests.
  */
 
+
+class Test {
+  constructor(val) {
+    this.string = val;
+    this.idc = 0;
+    this.selector = 0;
+    this.psel = 0;
+    this.prev = '';
+  }
+
+  element(value) {
+    this.selector += 1;
+    this.exception1();
+    this.exception2('element');
+    this.prev = 'element';
+    this.string += value;
+    return this;
+  }
+
+  stringify() {
+    return this.string;
+  }
+
+  id(value) {
+    this.idc += 1;
+    this.exception1();
+    this.exception2('element');
+    if (this.prev === 'psel') throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+
+    this.prev = 'id';
+
+    this.string += `#${value}`;
+    return this;
+  }
+
+  class(value) {
+    if (this.prev === 'attr') throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    this.prev = 'id';
+    this.string += `.${value}`;
+    return this;
+  }
+
+  attr(value) {
+    if (this.prev === 'pscl') throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+
+    this.prev = 'attr';
+    this.string += `[${value}]`;
+    return this;
+  }
+
+  pseudoClass(value) {
+    if (this.prev === 'psel') throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+
+    this.prev = 'pscl';
+    this.string += `:${value}`;
+    return this;
+  }
+
+  pseudoElement(value) {
+    this.prev = 'psel';
+
+    this.psel += 1;
+    this.exception1();
+
+
+    this.string += `::${value}`;
+
+    return this;
+  }
+
+  combine(selector1, combinator, selector2) {
+    const a = selector1;
+    const b = selector2;
+    this.string += `${a.stringify()} ${combinator} ${b.stringify()}`;
+    return this;
+  }
+
+  exception1() {
+    if (this.idc === 2 || this.selector === 2 || this.psel === 2) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+  }
+
+  exception2(value) {
+    if (this.prev !== value) {
+      if (this.prev !== '') {
+        throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+      }
+    }
+  }
+}
+
+
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  element(value) {
+    return new Test('').element(value);
+  },
+  id(value) {
+    return new Test('').id(value);
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    return new Test('').class(value);
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    return new Test('').attr(value);
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    return new Test('').pseudoClass(value);
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    return new Test('').pseudoElement(value);
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
-  },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    const test = new Test('');
+    return test.combine(selector1, combinator, selector2);
   },
 };
-
-
 module.exports = {
   Rectangle,
   getJSON,
